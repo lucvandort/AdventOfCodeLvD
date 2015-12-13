fc26f1fd1f07f81ab07af0ed7eed36a70bd73453
Luc van Dort <lucvandort@gmail.com>
2015-12-13 14:15:11 +0100

    Part 2 of Day 2!

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -44,6 +44,22 @@ def get_square_feet(dimensions):
     surface = 2*s1 + 2*s2 + 2*s3 + min(s1,s2,s3)
     
     return surface
+    
+def get_ribbon_length(dimensions):
+    l = dimensions['l']
+    w = dimensions['w']
+    h = dimensions['h']
+    
+    l1 = 2*(l+w)
+    l2 = 2*(w+h)
+    l3 = 2*(h+l)
+    
+    volume = l*w*h
+    
+    length = min(l1,l2,l3) + volume
+    
+    return length
+    
 
 #%% test functions
 testinput = {
@@ -66,13 +82,34 @@ for inputstring, squarefeet in testinput.items():
     
 assert(sum(testinput.values()) == sum(surfaces))
     
+testinput2 = {
+    "2x3x4": 34,
+    "1x1x10": 14,
+}
+
+lengths = []
+
+for inputstring, ribbonlength in testinput2.items():
+    dimensions = get_dimensions(inputstring)
+    length = get_ribbon_length(dimensions)
+    assert(ribbonlength == length)
+    lengths.append(length)    
+    
+assert(sum(testinput2.values()) == sum(lengths))
+    
 #%% import data
     
 total_square_feet = 0
+total_ribbon_length = 0
 with open('input.txt', 'r') as inputfile:
     for inputstring in inputfile:
         dimensions = get_dimensions(inputstring)
         surface = get_square_feet(dimensions)
+        length = get_ribbon_length(dimensions)
         total_square_feet += surface
+        total_ribbon_length += length
+
+#%%
+
 
 
