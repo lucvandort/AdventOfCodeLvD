5d56782bb96de40e0cf157b8aff2067c8c4ac252
Luc van Dort <Luc van Dort>
2015-12-20 22:30:54 +0100

    part 2 solved

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import matplotlib.pyplot as plt
+import pandas as pd
+
+plt.close('all')
 
 #%% 
 
@@ -35,16 +38,19 @@ def get_present_count(route_coordinates):
 
     return present_count
     
-#2nd exercise: 1 santa and 1 robotsanta take turns to deliver presents.
     
 #%% figure route
 
-def plot_route(route_coordinates):
+def plot_route(route_coordinates, route_coordinates2=None):
     fig = plt.figure()
     ax = plt.subplot(1,1,1)
     
     x,y = list(zip(*route_coordinates))
-    ax.plot(x,y)
+    ax.plot(x,y,'b')
+    
+    if route_coordinates2:
+        x2,y2 = list(zip(*route_coordinates2))
+        ax.plot(x2,y2,'r')
         
     fig.tight_layout()
 
@@ -65,8 +71,29 @@ for inputstring, houses in testinput.items():
 
 with open('input.txt', 'r') as inputfile:
     inputcode = inputfile.read()    
-        
+   
+#%% part 1
+     
 route_coordinates = get_route_coordinates(inputcode)
 present_count = get_present_count(route_coordinates)
 num_coordinates = len(present_count)
 plot_route(route_coordinates)
+
+#%% part 2
+
+inputcode_even = inputcode[0::2]
+inputcode_odd = inputcode[1::2]
+
+route_coordinates1 = get_route_coordinates(inputcode_even)
+route_coordinates2 = get_route_coordinates(inputcode_odd)
+plot_route(route_coordinates1, route_coordinates2)
+
+present_count1 = pd.DataFrame.from_dict(get_present_count(route_coordinates1), orient='index')
+present_count1.columns = ['Santa']
+present_count2 = pd.DataFrame.from_dict(get_present_count(route_coordinates2), orient='index')
+present_count2.columns = ['Robo Santa']
+
+present_count_total = pd.merge(present_count1, present_count2, how='outer', left_index=True, right_index=True)
+present_count_total = present_count_total.fillna(0)
+present_count_total['Total'] = present_count_total['Santa'] + present_count_total['Robo Santa']
+num_coordinates2 = len(present_count_total)
