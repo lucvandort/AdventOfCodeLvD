74a8cf34c2cd00017a982293251d626b318551f3
Luc van Dort <lucvandort@gmail.com>
2015-12-25 14:43:28 +0100

    Day 5 part 2

--- Day5/Day5.py
+++ Day5/Day5.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 #%% Definitions
 
@@ -17,20 +18,45 @@ def check_double_letter(inputstrings):
 def check_naughty_substring(inputstrings):
     return inputstrings.apply(lambda inputstring: True in [naughty_string in inputstring for naughty_string in NAUGHTY])
 
-
+def check_letter_repetition(inputstrings):
+    p = re.compile('(?P<letter>[a-z])[a-z](?P=letter)')
+    searchresults = [len(re.findall(p, inputstring)) for inputstring in inputstrings]
+    return searchresults
+    
+def check_letter_double(inputstrings):
+    p = re.compile('(?P<letters>[a-z]{2})[a-z]*(?P=letters)')
+    searchresults = [len(re.findall(p, inputstring)) for inputstring in inputstrings]
+    return searchresults
+    
 #%% Read input
 
 with open('input.txt', 'r') as inputfile:
     inputstrings = [line[:-1] for line in inputfile]
-    columns = ['Inputstring', 'Three vowels', 'Double letter', 'Naughty substring', 'Nice string']
-    data = pd.DataFrame(index=range(len(inputstrings)), columns=columns)
-    data['Inputstring'] = inputstrings
+    
+    columns1 = ['Inputstring', 'Three vowels', 'Double letter', 'Naughty substring', 'Nice string']
+    data1 = pd.DataFrame(index=range(len(inputstrings)), columns=columns1)
+    data1['Inputstring'] = inputstrings
+    
+    columns2 = ['Inputstring', 'Double letter repeat', 'Single letter repeat', 'Nice string']
+    data2 = pd.DataFrame(index=range(len(inputstrings)), columns=columns2)
+    data2['Inputstring'] = inputstrings
 
-#%% Process input
+#%% Part 1
 
-data['Three vowels'] = check_three_vowels(data['Inputstring'])
-data['Double letter'] = check_double_letter(data['Inputstring'])
-data['Naughty substring'] = check_naughty_substring(data['Inputstring'])
-data['Nice string'] = data.apply(lambda row: row['Three vowels'] and row['Double letter'] and not row['Naughty substring'], axis=1)
+data1['Three vowels'] = check_three_vowels(data1['Inputstring'])
+data1['Double letter'] = check_double_letter(data1['Inputstring'])
+data1['Naughty substring'] = check_naughty_substring(data1['Inputstring'])
+data1['Nice string'] = data1.apply(lambda row: row['Three vowels'] and row['Double letter'] and not row['Naughty substring'], axis=1)
 
-nice_string_count = len(data['Inputstring'].where(data['Nice string']).dropna())
+nice_string_count = len(data1['Inputstring'].where(data1['Nice string']).dropna())
+
+#%% Part 2
+
+data2['Single letter repeat'] = check_letter_repetition(data2['Inputstring'])
+data2['Double letter repeat'] = check_letter_double(data2['Inputstring'])
+
+data2['Nice string'] = data2.apply(lambda row: bool(row['Single letter repeat'] and row['Double letter repeat']), axis=1)
+
+nice_string2_count = len(data2['Inputstring'].where(data2['Nice string']).dropna())
+
+# SELECT `Inputstring` FROM `data2` WHERE `Nice string` = TRUE
