7714d7496aa48fcc4e44666491380c1f4d4b0d86
Luc van Dort <lucvandort@gmail.com>
2018-12-02 13:31:55 +0100

    solution for Day 1 completed.

--- 2018/Day 1/Day1.py
+++ 2018/Day 1/Day1.py
@@ -1,5 +1,31 @@
 import pandas as pd
+import numpy as np
+from numba import jit
 
 frequency_changes = pd.read_csv('input.txt', header=None)
 
+#%% part 1
+
 sum_of_frequency_changes = frequency_changes.sum(0)
+
+#%% part 2
+
+frequency_changes_array = np.array(frequency_changes.loc[:,0])
+
+@jit(nopython=True)
+def double_frequency_checker(frequency_changes_array, current_frequency=0):
+    frequency_counter = [current_frequency]
+    iteration_counter = 0
+    
+    while(True):
+        iteration_counter = iteration_counter + 1
+        print("Iteration", iteration_counter)
+        for df in frequency_changes_array:
+            current_frequency = current_frequency + df
+            if current_frequency in frequency_counter:
+                print("First double frequency at", current_frequency, "Hz")
+                return current_frequency
+            else:
+                frequency_counter.append(current_frequency)
+        
+first_double_frequency = double_frequency_checker(frequency_changes_array)
