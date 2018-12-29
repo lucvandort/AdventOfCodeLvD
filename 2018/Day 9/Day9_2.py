68956eb2a86f19789c740e490feaaa19cdc0be80
Luc van Dort <lucvandort@gmail.com>
2018-12-29 23:41:11 +0100

    alternative solution for Day 9 with a double ended queue

--- 2018/Day 9/Day9_2.py
+++ 2018/Day 9/Day9_2.py
@@ -1,5 +1,5 @@
 from tqdm import tqdm
-import numpy as np
+from collections import deque, defaultdict
 
 # %% Marblegable class definition
 
@@ -10,7 +10,7 @@ class Marblegame():
         self.num_players = num_players
         self.num_marbles = num_marbles
 
-        self.players = np.arange(1, self.num_players+1, dtype=int)
+        self.players = list(range(1, self.num_players+1))
         self.reset_game()
 
     def __str__(self):
@@ -20,50 +20,42 @@ class Marblegame():
         )
 
     def reset_game(self):
-        self.marbles = np.arange(1, self.num_marbles, dtype=int)
-        self.gamecircle = np.zeros(self.num_marbles, dtype=int)
-        self.scoreboard = {p: 0 for p in self.players}
+        self.marbles = list(range(0, self.num_marbles))
+        self.gamecircle = deque()
+        self.scoreboard = defaultdict(int)
         self.current_player = self.players[-1]
         self.current_marble = 0
+        self.gamecircle.append(self.marbles[self.current_marble])
         self.gamelength = 1
 
     def place_marble(self):
         try:
-            next_marble = self.marbles[0]
-            self.marbles = np.delete(self.marbles, 0)
+            next_marble = self.marbles[self.current_marble + 1]
         except IndexError:
             return -1
 
         self.current_player = self.players[
-            (np.where(self.players == self.current_player)[0][0] + 1)
+            (self.players.index(self.current_player) + 1)
             % self.num_players
         ]
 
         if next_marble % 23 == 0 and next_marble > 0:
             self.scoreboard[self.current_player] += next_marble
-            next_marble_index = \
-                (self.current_marble - 7) % self.gamelength
+            self.gamecircle.rotate(7)
             self.scoreboard[self.current_player] += \
-                self.gamecircle[next_marble_index]
-            self.gamecircle[next_marble_index:self.gamelength-1] = \
-                self.gamecircle[next_marble_index+1:self.gamelength]
-            self.gamelength -= 1
+                self.gamecircle.pop()
+            self.gamecircle.rotate(-1)
         else:
-            next_marble_index = \
-                (self.current_marble + 2) % self.gamelength
-            if next_marble_index == 0:
-                next_marble_index = self.gamelength
-            self.gamecircle[next_marble_index+1:self.gamelength+1] = \
-                self.gamecircle[next_marble_index:self.gamelength]
-            self.gamecircle[next_marble_index] = next_marble
-            self.gamelength += 1
+            self.gamecircle.rotate(-1)
+            self.gamecircle.append(next_marble)
 
-        self.current_marble = next_marble_index
+        self.gamelength += 1
+        self.current_marble = next_marble
         return self.current_marble
 
     def play_game(self):
         with tqdm(total=self.num_marbles) as pbar:
-            while len(self.marbles) > 0:
+            while self.gamelength < self.num_marbles:
                 self.place_marble()
                 pbar.update()
 
@@ -79,15 +71,18 @@ while testgame.place_marble() >= 0:
     print(testgame.current_player, testgame.gamecircle)
 print(testgame.scoreboard)
 print(testgame.highest_score())
+assert testgame.highest_score() == 32
 
 
-# %%
+# %% more testgames
+
 print("\n=== testgame 2 ===")
 testgame2 = Marblegame(num_players=10, num_marbles=1618+1)
 print(testgame2)
 testgame2.play_game()
 print(testgame2.scoreboard)
 print(testgame2.highest_score())
+assert testgame2.highest_score() == 8317
 
 print("\n=== testgame 3 ===")
 testgame3 = Marblegame(num_players=13, num_marbles=7999+1)
@@ -95,6 +90,7 @@ print(testgame3)
 testgame3.play_game()
 print(testgame3.scoreboard)
 print(testgame3.highest_score())
+assert testgame3.highest_score() == 146373
 
 print("\n=== testgame 4 ===")
 testgame4 = Marblegame(num_players=17, num_marbles=1104+1)
@@ -102,6 +98,7 @@ print(testgame4)
 testgame4.play_game()
 print(testgame4.scoreboard)
 print(testgame4.highest_score())
+assert testgame4.highest_score() == 2764
 
 print("\n=== testgame 5 ===")
 testgame5 = Marblegame(num_players=21, num_marbles=6111+1)
@@ -109,6 +106,7 @@ print(testgame5)
 testgame5.play_game()
 print(testgame5.scoreboard)
 print(testgame5.highest_score())
+assert testgame5.highest_score() == 54718
 
 print("\n=== testgame 6 ===")
 testgame6 = Marblegame(num_players=30, num_marbles=5807+1)
@@ -116,6 +114,8 @@ print(testgame6)
 testgame6.play_game()
 print(testgame6.scoreboard)
 print(testgame6.highest_score())
+assert testgame6.highest_score() == 37305
+
 
 # %% Part 1
 
@@ -137,13 +137,13 @@ part1_game.play_game()
 print(f"The winning Elf's score is {part1_game.highest_score()}.")
 
 # %% Part 2
-#print("\n=== Part 2 ===")
-#
-#part2_game = Marblegame(
-#    num_players=num_players,
-#    num_marbles=(last_marble_worth*100)+1,
-#)
-#print(part2_game)
-#part2_game.play_game()
-## print(part2_game.scoreboard)
-#print(f"The winning Elf's score is {part2_game.highest_score()}.")
+print("\n=== Part 2 ===")
+
+part2_game = Marblegame(
+    num_players=num_players,
+    num_marbles=(last_marble_worth*100)+1,
+)
+print(part2_game)
+part2_game.play_game()
+# print(part2_game.scoreboard)
+print(f"The winning Elf's score is {part2_game.highest_score()}.")
